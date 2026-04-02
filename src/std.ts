7b8d121ebd68e2bab65b13eccdd4c5b1561d10e2
Farhan Kabir <farhan@modlin.dev>
2026-04-03 00:35:41 +0600

    feat: added modlin international standards

--- src/std.ts
+++ src/std.ts
@@ -1,24 +1,24 @@
+export interface Connection {
+    hostname: string
+    port: number
+    auth: {
+        username: string
+        password: string
+    }
+}
+
 export enum Gender {
 	Male,
 	Female
 }
-export enum BloodGroup {
-	"A+",
-	"A-",
-	"B+",
-	"B-",
-	"O+",
-	"O-",
-	"AB+",
-	"AB-"
-}
-export interface Email {
+export type BloodGroup = "A+" | "A-" | "B+" | "B-" | "O+" | "O-" | "AB+" | "AB-"
+export interface EmailAddress {
 	displayname?: string
-	address: string // might be changed to "address"
+	address: string // "email" or "address" (unsure)
 }
 export interface Link {
-    label: string
-    url: string
+	label: string
+	url: string
 }
 export interface Address {
 	recipient: string
@@ -34,13 +34,21 @@ export interface Contact {
 	jobtitle?: string
 	department?: string
 	company?: string
-    about?: string
-    links: Link[]
+	about?: string
+	links: Link[]
 	phone: string[]
-	email: Email[] // in proposal of "string[]" or "Email[]"
+	email: EmailAddress[] // in proposal of "string[]" or "EmailAddress[]"
 	image?: string
 	birthday?: Date
 	gender?: Gender
 	bloodgroup?: BloodGroup // external for "Health and Safety Act."
 	address: Address[]
+}
+
+export interface User {
+    id: string
+    displayname?: string
+    username: string
+    created_at: Date
+    updated_at: Date
 }
