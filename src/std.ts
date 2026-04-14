41ea611deb48f1296d3bb29ee393a29990eb14e0
Farhan Kabir <farhan@modlin.dev>
2026-04-14 16:58:24 +0600

    add: standard type changes

--- src/std.ts
+++ src/std.ts
@@ -1,3 +1,4 @@
+export type ID = string
 export interface Connection {
     hostname: string
     port: number
@@ -46,9 +47,36 @@ export interface Contact {
 }
 
 export interface User {
-    id: string
+    id: ID
     displayname?: string
     username: string
     created_at: Date
     updated_at: Date
+}
+
+export interface Email {
+	id: ID
+	from: string
+	to: string[]
+	cc: string[]
+	bcc: string[]
+
+	subject: string
+	reply_to?: string[]
+	headers?: Record<string, string>
+	text?: string
+	html?: string
+	attachments: Attachment[]
+
+	message_id: string
+
+	created_at: string | Date
+}
+export interface Attachment {
+	id: ID
+	filename: string | null
+	content_type: string
+	content_disposition: string | null
+	content_id: string | null
+	content?: string | Buffer<ArrayBufferLike>
 }
